e7fc2e091f92603d1444a8c59482021af2d070c4
Leopoldo Caballero <git@pitakill.net>
2020-08-07 14:30:20 -0500

    Elixir machine

---
+++ provision/cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# VBox additions guest
+rm $HOME/VBoxGuestAdditions.iso
+
+# System
+sudo apt-get autoremove --purge
+sudo apt-get clean --dry-run
+sudo apt-get autoclean --dry-run
+sudo apt-get clean --dry-run
---
+++ provision/provision.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb
+sudo dpkg -i erlang-solutions_2.0_all.deb
+sudo apt-get update
+sudo apt-get install -y esl-erlang
+sudo apt-get install -y elixir
