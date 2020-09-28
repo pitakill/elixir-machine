43cbf3934883780c3145830afd8fb591bf952bc9
Leopoldo Caballero <git@pitakill.net>
2020-09-28 12:30:25 -0500

    Add curl, unzip, consul and consul-template binaries

--- provision/cleanup.sh
+++ provision/cleanup.sh
@@ -8,3 +8,6 @@ sudo apt-get autoremove --purge
 sudo apt-get clean --dry-run
 sudo apt-get autoclean --dry-run
 sudo apt-get clean --dry-run
+
+# Cleanup files
+rm $HOME/*.{deb,zip}
--- provision/provision.sh
+++ provision/provision.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+CONSUL_VERSION=1.7.3
+CONSUL_TEMPLATE_VERSION=0.24.0
 
+# Install elixir
 wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb
 sudo dpkg -i erlang-solutions_2.0_all.deb
 sudo apt-get update
-sudo apt-get install -y esl-erlang
-sudo apt-get install -y elixir
+sudo apt-get install -y esl-erlang \
+  elixir \
+  unzip \
+  curl
+
+# Install consul
+curl -sL https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -o consul.zip
+sudo unzip consul.zip
+sudo chmod +x consul
+sudo mv consul /usr/bin/consul
+
+# Install consul-template
+curl -sL https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip -o consul-template.zip
+sudo unzip consul-template.zip
+sudo chmod +x consul-template
+sudo mv consul-template /usr/bin/consul-template
